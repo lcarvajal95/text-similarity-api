088e63525b632876003f47e644a8606cc71d52b4
lcarvajal95 <lcarvajal95@gmail.com>
2025-05-02 10:03:09 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Puedes limitarlo luego a ["http://localhost:8000"] si prefieres
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
